77d053863abe1a9a238f67e592b99f98eaf6144e
RoboticSchools <91181914+RoboticSchools@users.noreply.github.com>
2025-04-04 21:00:55 +0530

    change

--- 1. Pi Learner Level/6. Traffic Light Simulation.py
+++ 1. Pi Learner Level/6. Traffic Light Simulation.py
@@ -2,7 +2,7 @@
 Components Used:
 - Raspberry Pi
 - Red LED (Stop)
-- Yellow LED (Ready)
+- Yellow LED (Slow Down)
 - Green LED (Go)
 - Breadboard
 - Jumper Wires
@@ -12,34 +12,34 @@ import RPi.GPIO as gpio
 import time
 
 # Pin configuration
-red_led = 21    # GPIO21 for Red LED (Stop)
+green_led = 21   # GPIO21 for Green LED (Go)
 yellow_led = 20  # GPIO20 for Yellow LED (Ready)
-green_led = 16   # GPIO16 for Green LED (Go)
+red_led = 16     # GPIO16 for Red LED (Stop)
 
 # GPIO setup
-gpio.setmode(gpio.BCM)  # Set pin numbering system to BCM
-gpio.setup(red_led, gpio.OUT)  # Set Red LED as output
-gpio.setup(yellow_led, gpio.OUT)  # Set Yellow LED as output
-gpio.setup(green_led, gpio.OUT)  # Set Green LED as output
+gpio.setmode(gpio.BCM)  # Use BCM pin numbering
+gpio.setup(red_led, gpio.OUT)
+gpio.setup(yellow_led, gpio.OUT)
+gpio.setup(green_led, gpio.OUT)
 
 try:
     while True:
-        gpio.output(red_led, gpio.HIGH)  # Red LED ON (Stop)
-        gpio.output(yellow_led, gpio.LOW)
-        gpio.output(green_led, gpio.LOW)
-        print("Red Light - STOP")
-        time.sleep(5)
-
-        gpio.output(red_led, gpio.LOW)
-        gpio.output(yellow_led, gpio.HIGH)  # Yellow LED ON (Ready)
-        gpio.output(green_led, gpio.LOW)
-        print("Yellow Light - READY")
-        time.sleep(2)
-
         gpio.output(red_led, gpio.LOW)
         gpio.output(yellow_led, gpio.LOW)
         gpio.output(green_led, gpio.HIGH)  # Green LED ON (Go)
         print("Green Light - GO")
+        time.sleep(5)
+
+        gpio.output(green_led, gpio.LOW)
+        gpio.output(yellow_led, gpio.HIGH)  # Yellow LED ON (Slow Down)
+        gpio.output(red_led, gpio.LOW)
+        print("Yellow Light - SLOW DOWN")
+        time.sleep(2)
+
+        gpio.output(yellow_led, gpio.LOW)
+        gpio.output(red_led, gpio.HIGH)  # Red LED ON (Stop)
+        gpio.output(green_led, gpio.LOW)
+        print("Red Light - STOP")
         time.sleep(5)
 
 except KeyboardInterrupt:
